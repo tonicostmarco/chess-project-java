41da62153beb65f86be179fb95c63604b68e3b69
Marco Antonio da Costa Ricardo <marcoricardo61b@gmail.com>
2025-10-06 18:50:22 -0300

    Chess layer and printing the board

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,13 +2,15 @@ package application;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        Board b = new Board(8, 8);
-      
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
+
     }
 }
