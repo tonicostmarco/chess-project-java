e5b93777ad8b636281b4068aa0d564c14c1c5991
Marco Antonio da Costa Ricardo <marcoricardo61b@gmail.com>
2025-10-06 18:02:00 -0300

    Position

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,12 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+    Position p = new Position(10, 10);
 
-
-
+        System.out.println(p);
     }
 }
